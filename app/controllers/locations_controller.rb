6823a93b02790f249888183ad05b1d4fe8a1711e
m724tmy <tomoya.724.a7x@icloud.com>
2024-11-19 23:50:15 +0900

    候補編集、削除機能の実装

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,6 @@
 class LocationsController < ApplicationController
     before_action :set_room
+    before_action :set_location, only: [:edit, :update, :show, :destroy]
 
     def new
         @location = @room.locations.build
@@ -16,7 +17,28 @@ class LocationsController < ApplicationController
     end
 
     def show
-        @location = @room.locations.find(params[:id])
+
+    end
+
+    def edit
+
+    end
+
+    def update
+        @location = @room.locations.build(location_params)
+        if @location.update(location_params)
+            redirect_to room_location_path(@room, @location), notice: '行きたい場所を更新しました'
+        else
+            render :edit, alert: '行きたい場所の更新に失敗しました'
+        end
+    end
+
+    def destroy
+        if @location.destroy
+            redirect_to room_path(@room), notice: 'スケジュールを削除しました'
+        else
+            redirect_to room_path(@room), alert: 'スケジュールの削除に失敗しました'
+        end
     end
 
     private
@@ -27,6 +49,10 @@ class LocationsController < ApplicationController
 
     def location_params
         params.require(:location).permit(:title, :start_time, :end_time, :description, :cost, :reference_url, :latitude, :longitude).merge(user_id: current_user.id)
+    end
+
+    def set_location
+        @location = @room.locations.find(params[:id])
     end
 
 end
