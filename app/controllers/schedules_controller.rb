1c7744e01a349830bd0f9022ee4ce066525194a5
m724tmy <tomoya.724.a7x@icloud.com>
2024-11-19 21:37:49 +0900

    しおり編集、削除機能の実装
    Update schedules_controller.rb

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,11 +1,12 @@
 class SchedulesController < ApplicationController
+    before_action :set_room
+    before_action :set_schedule, only: [:edit, :update, :show, :destroy]
+    
     def new
-        @room = Room.find(params[:room_id])
         @schedule = @room.schedules.build
     end
 
     def create
-        @room = Room.find(params[:room_id])
         @schedule = @room.schedules.build(schedule_params)
         if @schedule.save
             redirect_to room_path(@room), notice: "スケジュールを追加しました"
@@ -16,13 +17,44 @@ class SchedulesController < ApplicationController
     end
 
     def show
-        @room = Room.find(params[:room_id])
-        @schedule = @room.schedules.find(params[:id])
-      end
+
+    end
+
+    def edit
+
+    end
+    
+    def update
+        @schedule = @room.schedules.build(schedule_params)
+        if @schedule.update(schedule_params)
+            redirect_to room_schedule_path(@room, @schedule), notice: 'スケジュールを更新しました'
+        else
+            render :edit, alert: 'スケジュールの更新に失敗しました'
+        end
+    end
+
+    def destroy
+        # @schedule = @room.schedules.build(schedule_params)
+        if @schedule.destroy
+            redirect_to room_path(@room), notice: 'スケジュールを削除しました'
+        else
+            redirect_to room_path(@room), alert: 'スケジュールの削除に失敗しました'
+        end
+    end
+
 
     private
+    
+    def set_room
+        @room = Room.find(params[:room_id])
+    end
 
     def schedule_params
         params.require(:schedule).permit(:title, :start_time, :end_time, :description, :cost, :reference_url, :latitude, :longitude).merge(user_id: current_user.id)
     end
+
+    def set_schedule
+        @schedule = @room.schedules.find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :rooms do
-    resources :schedules, only: [:index, :new, :create, :edit, :update, :show]
+    resources :schedules
     resources :locations, only: [:index, :new, :create, :edit, :update]
     member do
       delete :leave
