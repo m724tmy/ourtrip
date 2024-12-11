1f13fb8601370b9fe863326d6cd4fa277e3c0962
m724tmy <tomoya.724.a7x@icloud.com>
2024-12-11 20:07:50 +0900

    環境変数設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
     skip_before_action :verify_authenticity_token
+    before_action :basic_auth
 
 
     private
@@ -12,4 +13,11 @@ class ApplicationController < ActionController::Base
     def set_room
         @room = Room.find(params[:room_id])
     end
+
+    def basic_auth
+        authenticate_or_request_with_http_basic do |username, password|
+          username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+        end
+    end
+
 end
