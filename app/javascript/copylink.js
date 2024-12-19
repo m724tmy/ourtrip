88786816233e5af2a5a9add832ffe8515a80059c
m724tmy <tomoya.724.a7x@icloud.com>
2024-12-20 07:03:02 +0900

    ユーザー招待機能修正

--- app/javascript/copylink.js
+++ app/javascript/copylink.js
@@ -1,6 +1,9 @@
-document.getElementById('copy-link').addEventListener('click', function () {
-  const link = this.getAttribute('data-link');
-  navigator.clipboard.writeText(link).then(() => {
-    alert('招待リンクをコピーしました！');
-  });
+document.addEventListener('click', function (event) {
+  const target = event.target;
+  if (target && target.id === 'copy-link') {
+    const link = target.getAttribute('data-link');
+    navigator.clipboard.writeText(link).then(() => {
+      alert('招待リンクをコピーしました！');
+    });
+  }
 });
